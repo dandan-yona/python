c4e58a2f04e04d404b80767b5a2e49f154bf8a4b
dandan-yona <121348051+dandan-yona@users.noreply.github.com>
2022-12-24 16:19:38 +0200

    first code

---
+++ main.py
@@ -0,0 +1,53 @@
+# a = 10
+# b = 2
+
+# print(a*b)
+
+# if a * b > 15:
+#     print("yes") 
+
+# else:
+#     print("no")
+
+
+
+# a = "daniel"
+# b = "elad"
+
+# if a == "elad":
+#     print("yes elad")
+
+# elif b == "elad":
+#     print("yes elad")   
+
+# elif a == "daniel":
+#     print("yes daniel")
+
+
+
+
+a = 7*1
+b = 7*2
+c = 7*3
+d = 7*4
+e = 7*5
+f = 7*6
+g = 7*7
+h = 7*8
+i = 7*9
+j = 7*10
+
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
+print(j)
+
+
+
