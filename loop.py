ecfeb682722df7f32c0a424e2cabc714b1f745b5
dandan-yona <121348051+dandan-yona@users.noreply.github.com>
2022-12-24 17:30:08 +0200

    loop example

---
+++ loop.py
@@ -0,0 +1,11 @@
+
+# Elad = "Elad"
+# names = [Elad,"Irit","Daniel","Hila","Lial"]
+
+# for name in names:
+#     print(name + " is cool")    
+
+numbers = [0,1,2,3,4,5,6]
+for number in numbers:
+    if number <= 5:
+        print(5 * number)  
